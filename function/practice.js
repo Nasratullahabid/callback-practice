456bfb33d0234e6ff594bfe837477ae05b92981d
Nasratullah <nasratullah.jssp@gmail.com>
2024-08-31 12:06:45 -0400

    CSS JS & Function is added to the project

---
+++ function/practice.js
@@ -0,0 +1,24 @@
+let uid=1001;
+
+let arr1=[10,20,30,40,50];
+
+let obj={
+          sid:101,
+          sname:'Jhon'
+        }
+
+function displayData(){
+    return "Jhon";
+}
+// let id = displayData();
+// console.log(id);
+
+function displayArray(){
+    return arr1;
+}
+// let arra= displayArray();
+// console.log(arra)
+
+function displayObject(){
+    return obj;
+}
